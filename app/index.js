953f22e98e999058a8b7f81f37108e4c87a5532f
Pierino <pmglplus@gmail.com>
2026-03-17 15:56:37 +0000

    dell

--- app/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>TESTING 123</h1>
-    </main>
-  );
-}
-                        
--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>TESTING 123</h1>
-    </main>
-  );
-}
