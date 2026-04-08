6dc2f585183018458b06332a5a1c6af7016f3fb5
Pierino <pmglplus@gmail.com>
2026-04-08 11:18:04 +0100

    mod exprt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
+
+module.exports = {
+  content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
